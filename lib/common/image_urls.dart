b0228c48fe4cb237005a12dff786f2f3313c3e01
Kushal Munjal <92504132+KushalMunjal@users.noreply.github.com>
2024-04-28 19:32:45 +0530

    added Global file for Image URL

---
+++ lib/common/image_urls.dart
@@ -0,0 +1,14 @@
+class ImageUrl{
+  String menu = "";
+  String offer = "";
+  String home = "";
+  String dinein= "";
+  String more = "";
+  String cart = "";
+  String search = "";
+  String cat_combo = "";
+  String cat_pizza = "";
+  String cat_burger = "";
+  String cat_desserts  = "";
+  String cat_beverages = "";
+}
